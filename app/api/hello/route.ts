62c2e896c207c0f079cfbbb78706182c8b810ec1
Samm <117623966+samm-waturu@users.noreply.github.com>
2023-06-07 06:51:00 +0300

    Update route.ts

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,26 +1,9 @@
-// This Next.js template already is configured to write with this Sanity Client
+import { NextResponse } from "next/server";
 
-import {previewClient} from '@/lib/sanity'
+export async function GET(request: Request) {
+  const json = {
+    message: "Hello World"
+  };
 
-export default async function createComment(req, res) {
-  // Destructure the pieces of our request
-  const { _id, name, email, comment} = JSON.parse(req.body)
-  try {
-    // Use our Client to create a new document in Sanity with an object  
-    await previewClient.create({
-      _type: 'comment',
-      post: {
-        _type: 'reference',
-        _ref: _id,
-      },
-      name,
-      email,
-      comment
-    })
-  } catch (err) {
-    console.error(err)
-    return res.status(500).json({message: `Couldn't submit comment`, err})
-  }
-
-  return res.status(200).json({ message: 'Comment submitted' })
+  return NextResponse.json(json);
 }
