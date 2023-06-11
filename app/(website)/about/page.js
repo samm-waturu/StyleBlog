2649f6a505ef005351043e4a434b681734e25e57
Samm <117623966+samm-waturu@users.noreply.github.com>
2023-06-11 22:30:54 +0300

    Prop error
    Finally fixed prop error

--- app/(website)/about/page.js
+++ app/(website)/about/page.js
@@ -3,8 +3,8 @@ import About from "./about";
 
 export default async function AboutPage() {
   const authors = await getAllAuthors();
-  // const settings = await getSettings();
-  return <About authors={authors} />;
+  const settings = await getSettings();
+  return <About settings={settings} authors={authors} />;
 }
 
 // export const revalidate = 60;
